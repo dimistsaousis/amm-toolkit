24bdd884285b3ffb5ee9e5a2b7425a854aa0ef65
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-27 22:07:35 +0300

    rename to pair address for consistency

--- src/amm/uniswap_v2/batch_request/mod.rs
+++ src/amm/uniswap_v2/batch_request/mod.rs
@@ -18,10 +18,11 @@ abigen!(
 );
 
 pub async fn get_uniswap_v2_pool_data_batch_request<M: Middleware>(
-    pool_address: H160,
+    pair_address: H160,
+    fee: u32,
     middleware: Arc<M>,
 ) -> Result<UniswapV2Pool, AMMError<M>> {
-    let constructor_args = Token::Tuple(vec![Token::Array(vec![Token::Address(pool_address)])]);
+    let constructor_args = Token::Tuple(vec![Token::Array(vec![Token::Address(pair_address)])]);
     let deployer = IGetUniswapV2PoolDataBatchRequest::deploy(middleware.clone(), constructor_args)?;
     let return_data: Bytes = deployer.call_raw().await?;
     let return_data_tokens = ethers::abi::decode(
@@ -39,56 +40,56 @@ pub async fn get_uniswap_v2_pool_data_batch_request<M: Middleware>(
     let tokens = return_data_tokens
         .into_iter()
         .next()
-        .ok_or(AMMError::<M>::BatchRequestError(pool_address))
+        .ok_or(AMMError::<M>::BatchRequestError(pair_address))
         .unwrap()
         .into_array()
-        .ok_or(AMMError::<M>::BatchRequestError(pool_address))
+        .ok_or(AMMError::<M>::BatchRequestError(pair_address))
         .unwrap()
         .into_iter()
         .next()
-        .ok_or(AMMError::<M>::BatchRequestError(pool_address))
+        .ok_or(AMMError::<M>::BatchRequestError(pair_address))
         .unwrap()
         .into_tuple()
-        .ok_or(AMMError::<M>::BatchRequestError(pool_address))
+        .ok_or(AMMError::<M>::BatchRequestError(pair_address))
         .unwrap();
 
     let pool = UniswapV2Pool {
         token_a: tokens[0]
             .to_owned()
             .into_address()
-            .ok_or(AMMError::<M>::BatchRequestError(pool_address))
+            .ok_or(AMMError::<M>::BatchRequestError(pair_address))
             .unwrap(),
         token_a_decimals: tokens[1]
             .to_owned()
             .into_uint()
-            .ok_or(AMMError::<M>::BatchRequestError(pool_address))
+            .ok_or(AMMError::<M>::BatchRequestError(pair_address))
             .unwrap()
             .as_u32() as u8,
         token_b: tokens[2]
             .to_owned()
             .into_address()
-            .ok_or(AMMError::<M>::BatchRequestError(pool_address))
+            .ok_or(AMMError::<M>::BatchRequestError(pair_address))
             .unwrap(),
         token_b_decimals: tokens[3]
             .to_owned()
             .into_uint()
-            .ok_or(AMMError::<M>::BatchRequestError(pool_address))
+            .ok_or(AMMError::<M>::BatchRequestError(pair_address))
             .unwrap()
             .as_u32() as u8,
         reserve_0: tokens[4]
             .to_owned()
             .into_uint()
-            .ok_or(AMMError::<M>::BatchRequestError(pool_address))
+            .ok_or(AMMError::<M>::BatchRequestError(pair_address))
             .unwrap()
             .as_u128(),
         reserve_1: tokens[5]
             .to_owned()
             .into_uint()
-            .ok_or(AMMError::<M>::BatchRequestError(pool_address))
+            .ok_or(AMMError::<M>::BatchRequestError(pair_address))
             .unwrap()
             .as_u128(),
-        address: pool_address,
-        fee: 0,
+        address: pair_address,
+        fee,
     };
 
     Ok(pool)
@@ -108,18 +109,19 @@ mod tests {
         dotenv::dotenv().ok();
         let rpc_endpoint = std::env::var("NETWORK_RPC").expect("Missing NETWORK_RPC env variable");
         let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint).unwrap());
-        let uniswap_v2_usdc_weth_pool_address =
+        let uniswap_v2_usdc_weth_pair_address =
             H160::from_str("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc").unwrap();
 
         let result = get_uniswap_v2_pool_data_batch_request(
-            uniswap_v2_usdc_weth_pool_address,
+            uniswap_v2_usdc_weth_pair_address,
+            300,
             middleware.clone(),
         )
         .await;
 
         match result {
             Ok(pool) => {
-                assert_eq!(pool.address, uniswap_v2_usdc_weth_pool_address);
+                assert_eq!(pool.address, uniswap_v2_usdc_weth_pair_address);
                 assert_eq!(
                     pool.token_a,
                     H160::from_str("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48").unwrap()
@@ -132,7 +134,7 @@ mod tests {
                 assert_eq!(pool.token_b_decimals, 18);
                 assert!(pool.reserve_0 > 0);
                 assert!(pool.reserve_1 > 0);
-                assert!(pool.fee == 0);
+                assert!(pool.fee == 300);
             }
             Err(e) => panic!("Error: {:?}", e),
         }
