23518e3c2aabf08f35450966714208449f385d89
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-28 18:23:45 +0300

    nit formatting

--- src/playground.rs
+++ src/playground.rs
@@ -9,7 +9,8 @@ use crate::amm::uniswap_v2::{
 };
 
 pub async fn simulate_swaps() -> eyre::Result<()> {
-    let rpc_endpoint = std::env::var("NETWORK_RPC").expect("Missing NETWORK_RPC env variable");
+    let rpc_endpoint: String =
+        std::env::var("NETWORK_RPC").expect("Missing NETWORK_RPC env variable");
     let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint)?);
     let uniswap_v2_usdc_weth_pool_address =
         H160::from_str("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc")?;
