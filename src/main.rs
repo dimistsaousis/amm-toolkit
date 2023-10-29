7d4e028471207df57032ffb69c3981bad1100e43
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-29 22:56:36 +0200

    refactoring main

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,6 @@ async fn main() {
     playground::simulate_swaps().await.unwrap();
     playground::get_usdc_weth_price().await.unwrap();
     playground::get_swap_call_data().await.unwrap();
-    playground::get_pairs_of_uniswap_v2_factory().await.unwrap();
-    playground::get_all_uniswap_v2_pools_for_block_from_logs()
-        .await
-        .unwrap();
-    // playground::compare_concurrent_and_serial_uniswap2_pool_sync()
-    //     .await
-    //     .unwrap();
-    playground::get_sync_all_uniswap_v2_pairs_from_logs()
-        .await
-        .unwrap();
+    playground::get_pools_from_log().await.unwrap();
+    playground::get_all_pools().await.unwrap();
 }
