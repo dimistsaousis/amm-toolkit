c202961b10b44dbc1159a5469b5b93a6c38e9f79
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-29 22:15:57 +0200

    get_sync_all_uniswap_v2_pairs_from_logs

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,13 @@ async fn main() {
     playground::get_usdc_weth_price().await.unwrap();
     playground::get_swap_call_data().await.unwrap();
     playground::get_pairs_of_uniswap_v2_factory().await.unwrap();
-    playground::get_sync_uniswap_v2_pools().await.unwrap();
     playground::get_all_uniswap_v2_pools_for_block_from_logs()
+        .await
+        .unwrap();
+    // playground::compare_concurrent_and_serial_uniswap2_pool_sync()
+    //     .await
+    //     .unwrap();
+    playground::get_sync_all_uniswap_v2_pairs_from_logs()
         .await
         .unwrap();
 }
