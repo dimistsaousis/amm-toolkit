7534894fddcbec37ae6a49aa670423630bd508ae
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-31 00:04:14 +0200

    simplify playground calls

--- src/playground.rs
+++ src/playground.rs
@@ -1,99 +1,94 @@
-use ethers::{
-    providers::{Http, Provider},
-    types::{H160, U256},
-};
-use std::{str::FromStr, sync::Arc};
+use ethers::types::{H160, U256};
+use std::str::FromStr;
 
-use crate::amm::uniswap_v2::{
-    batch_request::get_weth_value_in_pools, factory::UniswapV2Factory, sync::sync_uniswap_v2_pools,
-    UniswapV2Pool,
+use crate::{
+    amm::uniswap_v2::{
+        batch_request::get_weth_value_in_pools, sync::sync_uniswap_v2_pools, UniswapV2Pool,
+    },
+    configs::Config,
 };
 
 pub async fn simulate_swaps() -> eyre::Result<()> {
-    let rpc_endpoint: String =
-        std::env::var("NETWORK_RPC").expect("Missing NETWORK_RPC env variable");
-    let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint)?);
-    let uniswap_v2_usdc_weth_pool_address =
-        H160::from_str("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc")?;
-    let pool =
-        UniswapV2Pool::new_from_address(uniswap_v2_usdc_weth_pool_address, 300, middleware).await?;
-    let token_in = H160::from_str("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")?;
-    let amount_out = pool.simulate_swap(token_in, U256::from_dec_str("1000000000000000000")?)?;
+    let config = Config::new()?;
+    let pool = UniswapV2Pool::new_from_address(
+        config.uniswap_v2_pairs["WETH"]["USDc"],
+        300,
+        config.middleware,
+    )
+    .await?;
+    let amount_out = pool.simulate_swap(
+        config.tokens["WETH"],
+        U256::from_dec_str("1000000000000000000")?,
+    )?;
     println!("Amount out: {amount_out}");
     Ok(())
 }
 
 pub async fn get_usdc_weth_price() -> eyre::Result<()> {
-    let rpc_endpoint = std::env::var("NETWORK_RPC").expect("Missing NETWORK_RPC env variable");
-    let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint)?);
-    let uniswap_v2_usdc_weth_pool_address =
-        H160::from_str("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc")?;
-    let pool =
-        UniswapV2Pool::new_from_address(uniswap_v2_usdc_weth_pool_address, 300, middleware).await?;
-    let token_in = H160::from_str("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")?;
-    let price = pool.calculate_price(token_in)?;
+    let config = Config::new()?;
+    let price = config
+        .pool("WETH", "USDc")
+        .await?
+        .calculate_price(config.tokens["WETH"])?;
     println!("ETH/USD: {price}");
     Ok(())
 }
 
 pub async fn get_swap_call_data() -> eyre::Result<()> {
-    let rpc_endpoint = std::env::var("NETWORK_RPC")?;
-    let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint)?);
-    let uniswap_v2_usdc_weth_pool_address =
-        H160::from_str("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc")?;
-    let pool =
-        UniswapV2Pool::new_from_address(uniswap_v2_usdc_weth_pool_address, 300, middleware).await?;
+    let config = Config::new()?;
     let to_address = H160::from_str("0xdfC9F6651b27Fe326D61052979e6d8b90774240d")?;
-    let swap_calldata = pool.swap_calldata(U256::from(10000), U256::zero(), to_address, vec![]);
+    let swap_calldata = config.pool("WETH", "USDc").await?.swap_calldata(
+        U256::from(10000),
+        U256::zero(),
+        to_address,
+        vec![],
+    )?;
     println!("Swap calldata: {:?}", swap_calldata);
     Ok(())
 }
 
 pub async fn get_pools_from_log() -> eyre::Result<()> {
-    let rpc_endpoint = std::env::var("NETWORK_RPC")?;
-    let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint)?);
-    let uniswap_v2_factory = H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")?;
-    let factory = UniswapV2Factory::new(uniswap_v2_factory, 10000835, 300);
-    let pools = factory
-        .get_pools_from_logs(middleware, Some(10008355), Some(10008355 + 1000), None)
+    let config = Config::new()?;
+    let pools = config
+        .uniswap_v2_factory
+        .get_pools_from_logs(
+            config.middleware,
+            Some(10008355),
+            Some(10008355 + 1000),
+            None,
+        )
         .await?;
     println!("Got following pools from blocks: {:?}", pools);
     Ok(())
 }
 
 pub async fn get_all_pools() -> eyre::Result<()> {
-    let rpc_endpoint = std::env::var("NETWORK_RPC")?;
-    let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint)?);
-    let uniswap_v2_factory = H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")?;
-    let factory = UniswapV2Factory::new(uniswap_v2_factory, 10000835, 300);
-    let pools = factory.get_all_pools(middleware, None).await?;
+    let config = Config::new()?;
+    let pools = config
+        .uniswap_v2_factory
+        .get_all_pools(config.middleware, None)
+        .await?;
     println!("Got {:?}", pools.0.len());
     Ok(())
 }
 
 pub async fn run_sync_uniswap_v2_pools() -> eyre::Result<()> {
-    let rpc_endpoint = std::env::var("NETWORK_RPC")?;
-    let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint)?);
-    let uniswap_v2_factory = H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")?;
-    let factory = UniswapV2Factory::new(uniswap_v2_factory, 10000835, 300);
-    let pools = sync_uniswap_v2_pools(factory, middleware).await?;
+    let config = Config::new()?;
+    let pools = sync_uniswap_v2_pools(config.uniswap_v2_factory, config.middleware).await?;
     println!("Got {:?}", pools.len());
     Ok(())
 }
 
 pub async fn get_top10_pools_in_terms_of_weth_equivalent_value() -> eyre::Result<()> {
-    let rpc_endpoint = std::env::var("NETWORK_RPC")?;
-    let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint)?);
-    let uniswap_v2_factory = H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")?;
-    let factory = UniswapV2Factory::new(uniswap_v2_factory, 10000835, 300);
-    let weth_address = H160::from_str("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")?;
-    let pools = sync_uniswap_v2_pools(factory, middleware.clone()).await?;
+    let config = Config::new()?;
+    let pools =
+        sync_uniswap_v2_pools(config.uniswap_v2_factory.clone(), config.middleware.clone()).await?;
     let pool_addresses = pools.into_iter().map(|pool| pool.address).collect();
     let weth_values_in_pools_batch = get_weth_value_in_pools(
         pool_addresses,
-        weth_address,
-        uniswap_v2_factory,
-        middleware.clone(),
+        config.tokens["WETH"],
+        config.uniswap_v2_factory.address,
+        config.middleware.clone(),
         Some(50),
     )
     .await?;
